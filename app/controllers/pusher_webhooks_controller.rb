09038096c32a88caf3437ff1b7d957ff4c95d08a
Shane Perreault <shaneprrlt@gmail.com>
2017-04-26 14:04:26 -0500

    removed unused controller

--- app/controllers/pusher_webhooks_controller.rb
+++
@@ -1,2 +0,0 @@
-class PusherWebhooksController < ApplicationController
-end
--- test/controllers/pusher_webhooks_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PusherWebhooksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
